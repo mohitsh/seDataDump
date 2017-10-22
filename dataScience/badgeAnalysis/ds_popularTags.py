b1bef7519ce6a3e7d943371850ae383b40ad56d5
mohitsh114 <mohitsh114@gmail.com>
2017-10-23 01:38:02 +0530

    most awarded badges

--- dataScience/badgeAnalysis/ds_popularTags.py
+++ dataScience/badgeAnalysis/ds_popularTags.py
@@ -7,13 +7,23 @@ import fetchData as fd
 
 def processor():
     posts = fd.table_reader('posts')
+    badges = fd.table_reader('badges')
+
+    # most used tags in posts, threshold is 25
     ptags = posts.Tags.value_counts()
     ptags = ptags[ptags>25]
-    print(ptags)
-    ptags.plot.pie(autopct="%.2f", fontsize=20, figsize=(12,12))
+
+    # most awarded badges, threshold is 1000
+    bnames = badges.Name.value_counts()
+    bnames = bnames[bnames>1000]
+
+    #ptags.plot.pie(autopct="%.2f", fontsize=20, figsize=(12,12))
+    #plt.tight_layout()
+    
+    print(bnames)
+    bnames.plot.pie(autopct='%.2f', fontsize=20, figsize=(15, 15))
     plt.tight_layout()
     plt.show()
-
 
 
 if __name__ == "__main__":
